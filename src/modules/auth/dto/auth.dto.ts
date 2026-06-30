352b2332b706be6448b83482ae5484225591c213
M Mudasir <muhammedmudassir40@gmail.com>
2026-06-30 06:58:10 -0700

    reset password

--- src/modules/auth/dto/auth.dto.ts
+++ src/modules/auth/dto/auth.dto.ts
@@ -27,3 +27,24 @@ export class LoginDto {
   @IsString()
   password: string;
 }
+
+export class ForgotPasswordDto {
+  @IsEmail({}, { message: 'Invalid email format' })
+  email: string;
+
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  password: string;
+}
+
+export class ResetPasswordDto {
+  @IsEmail({}, { message: 'Invalid email format' })
+  email: string;
+
+  @IsString()
+  token: string;
+
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  password: string;
+}
